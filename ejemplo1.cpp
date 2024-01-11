0726a88f32d536435a22fb94f6cc467473aacbd1
douglasDFH <155786486+douglasDFH@users.noreply.github.com>
2024-01-10 21:18:03 -0400

    Add files via upload

---
+++ ejemplo1.cpp
@@ -0,0 +1,23 @@
+
+/* 
+titulo : primer programa con comentarios
+autor: juan david meza gonzales 
+descripciom : ver programa para analizar su estructura.
+fecha : 2024-01-10
+*/
+#include <iostream> // inclucion de la libreria de entrada y salida
+using namespace std;// uso del epacio de nombres para el std de la i/o
+int main ()//funcion principal main para retornar un entero.
+{ //funcion principal main para retornar un entero.
+	
+	int numero ; // declaracionde de la variable numero como entero 
+	
+	numero = 5 + 5 ;// asignacion de la suma de 5+5 a la variable numero.
+	/*
+	la impresion del resultado de la variable numero
+	y adicion al final del salto de linea (endl)
+	*/
+	cout<<"el resultado de la suma es :"<<numero; 
+	// impresion de la variable numero 
+	return (0);// retorno de 0 para finalizar el programa
+}
